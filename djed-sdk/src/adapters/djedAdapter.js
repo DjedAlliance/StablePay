cd23797aa939ed04911f45912a7bde75915f95d0
Pragati <pragatimittal31@gmail.com>
2026-03-18 11:49:29 +0530

    refactor: improve djed adapter abstraction

--- djed-sdk/src/adapters/djedAdapter.js
+++ djed-sdk/src/adapters/djedAdapter.js
@@ -1,15 +1,24 @@
 import { FEE_UI_UNSCALED } from "../djed/tradeUtils";
 
 export const createDjedAdapter = (djedContract) => {
-  if (!djedContract || !djedContract.methods) {
-    throw new Error("Djed contract instance is required");
+  const methods = djedContract?.methods;
+
+  // Fail fast if required methods are missing
+  if (
+    !methods ||
+    typeof methods.buyStableCoins !== "function" ||
+    typeof methods.sellStableCoins !== "function"
+  ) {
+    throw new Error(
+      "Invalid Djed contract instance: missing required methods (buyStableCoins, sellStableCoins)"
+    );
   }
 
   return {
     buyStableCoins: (receiver, UI) =>
-      djedContract.methods.buyStableCoins(receiver, FEE_UI_UNSCALED, UI),
+      methods.buyStableCoins(receiver, FEE_UI_UNSCALED, UI),
 
     sellStableCoins: (amount, account, UI) =>
-      djedContract.methods.sellStableCoins(amount, account, FEE_UI_UNSCALED, UI),
+      methods.sellStableCoins(amount, account, FEE_UI_UNSCALED, UI),
   };
 };
